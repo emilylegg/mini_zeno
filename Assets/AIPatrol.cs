7e93ed3dfdf2cedd87065da6a8a39b26fe64546d
Emily <pinkpanther456.el@gmail.com>
2022-04-27 03:29:53 -0400

    added main menu

---
+++ Assets/AIPatrol.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIPatrol : MonoBehaviour
+{
+    // Start is called before the first frame updatez
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
